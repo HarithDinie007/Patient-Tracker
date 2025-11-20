0e940fbe66ee7c9acd2faa8d08565bf7300d1fe4
Harith <96896253+HarithDinie007@users.noreply.github.com>
2025-11-20 10:32:16 +0800

    Integrate Telegram notifications for device alerts
    Added Telegram notification functionality for alerts.

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import requests
+
+BOT_TOKEN = "8391197716:AAEceZ47cupC7odBuGzAYms4mRdMtStEnMA"
+CHAT_ID = "-1003413811334"
+
+TELE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+def send_telegram_message(text):
+    requests.post(TELE_URL, json={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
+
+def run_action(device_id):
+    print(f">>> Taking action for {device_id}!")
+    text = f"ALERT: {device_id} is leaving the facility!"
+    send_telegram_message(text)
+    
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers['Content-Length'])
         data = self.rfile.read(length).decode()
-
-        print(data)
 
         # -------------------------
         # Parse device ID
@@ -29,9 +45,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-
-def run_action(device_id):
-    print(f">>> Taking action for {device_id}!")
 
 server = HTTPServer(("0.0.0.0", 8080), Handler)
 print("Server running on port 8080...")
