b37bfb7430ce7b1862f1bb2bce2e276a4b7ede39
Harith <96896253+HarithDinie007@users.noreply.github.com>
2025-11-26 20:32:48 +0800

    Implement device state tracking and alert limits
    Added device state management and alert burst control.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import requests
+import requests, time
 
 BOT_TOKEN = "8391197716:AAEceZ47cupC7odBuGzAYms4mRdMtStEnMA"
 CHAT_ID = "-1003413811334"
 
 TELE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+device_state = {}
+
+GAP_SECONDS = 5
+BURST_LIMIT = 5
 
 def send_telegram_message(text):
     requests.post(TELE_URL, json={
@@ -24,6 +29,8 @@ def fall_alert(device_id, distance):
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
+        global device_state
+        
         length = int(self.headers['Content-Length'])
         data = self.rfile.read(length).decode()
 
@@ -32,7 +39,6 @@ class Handler(BaseHTTPRequestHandler):
         # -------------------------
         device_id = "Unknown"
         distance = 0
-        alert_type = "None"
 
         # Split by semicolon
         parts = data.split(";")
@@ -40,25 +46,45 @@ class Handler(BaseHTTPRequestHandler):
         device_id = parts[0].split("=")[1]
         # Extract distance
         distance = int(parts[1].split("=")[1])
-        # Extract alert type
-        alert_type = parts[2].split("=")[1]
 
-        print(device_id, distance)
+
+        print(data)
+        
+        now = time.time()
+        
+        if device_id not in device_state:
+            device_state[device_id] = {"count": 0, "last_time": now}
+
+        time_gap = now - device_state[device_id]["last_time"]
+
+        if time_gap > GAP_SECONDS:
+            print(f"[RESET] Gap detected for {device_id}, resetting counter")
+            device_state[device_id]["count"] = 0
+
+        device_state[device_id]["last_time"] = now
 
         # -------------------------
         # React to alert
         # -------------------------
-        if "LEAVING" in data:
-            print(f"ALERT RECEIVED from {device_id}! Running action...")
-            #leaving_alert(device_id, distance)
-
+        
+        count = device_state[device_id]["count"]
+        
         if "FALL" in data:
-            print(f"FALL ALERT RECEIVED from {device_id}! Running action...")
-            #fall_alert(device_id, distance)
+                print(f"FALL ALERT RECEIVED from {device_id}! Running action...")
+                fall_alert(device_id, distance)
+        
+        if count == 0:
+            if "LEAVING" in data:
+                print(f"ALERT RECEIVED from {device_id}! Running action...")
+                leaving_alert(device_id, distance)
+                
+        else:
+            print(f"Skipping alert for {device_id} (count = {count})")
+            
+        device_state[device_id]["count"] = (count + 1) % BURST_LIMIT
 
         self.send_response(200)
         self.end_headers()
-
 
 server = HTTPServer(("0.0.0.0", 8080), Handler)
 print("Server running on port 8080...")
